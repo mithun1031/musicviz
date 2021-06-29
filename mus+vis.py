7eb8e547f34c1008093cf5b352b61275cae355bc
Mithun M <67685518+mithun1031@users.noreply.github.com>
2021-06-29 22:30:17 +0530

    Add files via upload

---
+++ mus+vis.py
@@ -0,0 +1,20 @@
+# Import everything needed to edit video clips
+from moviepy.editor import *
+
+
+# loading video dsa gfg intro video
+clip = VideoFileClip("abcd.mp4")
+
+
+# getting only first 5 seconds
+#clip = clip.subclip(0, 5)
+
+# loading audio file
+audioclip = AudioFileClip("music.mp3")
+#.subclip(0, 5)
+
+# adding audio to the video clip
+videoclip = clip.set_audio(audioclip)
+
+# showing video clip
+videoclip.ipython_display()
